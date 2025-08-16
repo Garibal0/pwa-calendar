30547c8210822aa91b78cb1b135a47ccb5ec2dcb
unknown <a2e3xd@gmail.com>
2025-08-16 21:28:53 +0200

    Initial commit - PWA Calendar

---
+++ app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0uoslmSpHIWBLwsMLP8wuOpoQkh9tB7c",
+  uthDomain: "grzybek-3ded4.firebaseapp.com",
+  projectId: "grzybek-3ded4",
+  storageBucket: "grzybek-3ded4.firebasestorage.app",
+  messagingSenderId: "318728205473",
+  appId: "1:318728205473:web:3d7e110088ff1e2c7aace0",
+  measurementId: "G-K3L05XR8KH"
+};
+
+console.log("PWA Calendar loaded");
